d7ab3eb3a048338f49c1c75e42c1925edc79a258
Shristisingh14 <84385618+Shristisingh14@users.noreply.github.com>
2022-08-24 16:51:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+
+}
+function take_snapshot() {
+    save('Clown_nose_image.png');
+}
